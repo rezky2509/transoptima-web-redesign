ddda97e11bdf0a4f0b8c632c3ba36ca2e5e76253
rezky2509 <rezkydwiafian@gmail.com>
2026-04-27 23:29:12 +0800

    changes in configuration

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
-const isGitHubPages = process.env.GITHUB_ACTIONS === "true" && !!repoName;
+// const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+// const isGitHubPages = process.env.GITHUB_ACTIONS === "true" && !!repoName;
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isGitHubPages ? `/${repoName}` : undefined,
-  assetPrefix: isGitHubPages ? `/${repoName}/` : undefined,
+  // basePath: isGitHubPages ? `/${repoName}` : undefined,
+  // assetPrefix: isGitHubPages ? `/${repoName}/` : undefined,
   images: {
     unoptimized: true,
   },
